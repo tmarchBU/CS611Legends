e139e521052fc9610aebab7a42382436bf8b37dd
Tristan Marchand <tmarch@bu.edu>
2020-11-23 15:57:30 -0500

    max characters per cell to 1

--- Legends/game/rules/LegendsValorRules.java
+++ Legends/game/rules/LegendsValorRules.java
@@ -3,7 +3,7 @@ package game.rules;
 public class LegendsValorRules 
 {
     public static double BUSHCELL_DEXTERITY_INCREASE = 0.1;   
-    public static int MAX_CHARACTERS_PER_CELL = 2;
+    public static int MAX_CHARACTERS_PER_CELL = 1;
     public static double CAVECELL_AGILITY_INCREASE = 0.1;   
     public static double KOULOUCELL_STRENGTH_INCREASE = 0.1;
     public static int NUM_LANES = 3;
