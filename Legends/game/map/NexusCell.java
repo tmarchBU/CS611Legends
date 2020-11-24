3ac510d91fdeb9caa42bd0d88c0cea25463a9a6c
Tristan Marchand <tmarch@bu.edu>
2020-11-23 22:17:32 -0500

    deleted more useless code

--- Legends/game/map/NexusCell.java
+++ Legends/game/map/NexusCell.java
@@ -14,12 +14,10 @@ Description: Subclass of cell, representing a Nexus cell. Used for Legends Valor
 Imported Libraries
 */
 import GUI_helper.*;
-import game.Nexus;
 
 public class NexusCell extends Cell
 {
     public static final Marker marker = new Marker(TextColors.YELLOW + Icons.MARKET_ICON + " " + TextColors.RESET);
-    private Nexus nexus;
 
     /*
     CONSTRUCTORS 
@@ -27,15 +25,5 @@ public class NexusCell extends Cell
     public NexusCell()
     {
         super(marker);
-    }
-
-    public void setNexus(Nexus nexus)
-    {
-        this.nexus = nexus;
-    }
-
-    public Nexus getNexus()
-    {
-        return nexus;
     }
 }
